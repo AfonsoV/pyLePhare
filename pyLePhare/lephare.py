11e39f05772e3ed1fb118a0fb21b1167fbb63da3
AfonsoV <bribeiro@Brunos-MacBook-Pro.local>
2019-06-14 15:52:18 +0200

    added run command functionality to class

--- pyLePhare/lephare.py
+++ pyLePhare/lephare.py
@@ -43,23 +43,24 @@ class LePhare:
         if config is not None:
             assert isinstance(config, dict), "config must be a dictionary."
             for key, val in config.items():
-                if key not in lePhare.CONFIG.keys():
+                if key not in LePhare.CONFIG.keys():
                     raise lephareError(f"{key} not valid in config file")
                 else:
-                    lePhare.CONFIG[key] = val
+                    LePhare.CONFIG[key] = val
 
         if parameters is not None:
             assert isinstance(parameters, dict), "parameters must be a dictionary."
             for key, val in parameters.items():
-                if key not in lePhare.PARAMETERS.keys():
+                if key not in LePhare.PARAMETERS.keys():
                     raise lephareError(f"{key} not valid in parameters file")
                 else:
-                    lePhare.PARAMETERS[key] = val
+                    LePhare.PARAMETERS[key] = val
         return
 
 
     def set_working_directory(self,dir):
         os.environ["LEPHAREWORK"] = dir
+        self.workdir = dir
         return
 
     def run_cmd(self,cmd,debug=False):
@@ -90,6 +91,47 @@ class LePhare:
         cmd = f"$LEPHAREDIR/source/zphota -c {parfile}"
         result = self.run_cmd(cmd,debug=debug)
         return result
+
+    def prepare_files(self,parfile):
+
+        if not os.path.isdir(workdir):
+            os.mkdir(workdir)
+
+        f = open(f"{self.workdir}/{parfile}","w")
+        for key, val in LePhare.CONFIG.items():
+            f.write(f"{key}\t{val}\n")
+        f.close()
+
+        f = open(f"{LePhare.CONFIG['PARA_OUT']}","w")
+        for key, val in LePhare.PARAMETERS.items():
+            f.write(f"{key}\t{val}\n")
+        f.close()
+
+        return
+
+    def run_all(self,parfile,debug=False):
+        self.prepare_files(parfile)
+
+        self.sedtolib("S",parfile,debug=debug)
+        self.sedtolib("Q",parfile,debug=debug)
+        self.sedtolib("G",parfile,debug=debug)
+
+        self.filter(parfile,debug=debug)
+
+        self.mag("S",parfile,debug=debug)
+        self.mag("Q",parfile,debug=debug)
+        self.mag("G",parfile,debug=debug)
+
+        self.zphota(parfile,debug=debug)
+        return
+
+    def run_gal(self,parfile,debug=False):
+        self.prepare_files(parfile)
+        self.sedtolib("G",parfile,debug=debug)
+        self.filter(parfile,debug=debug)
+        self.mag("G",parfile,debug=debug)
+        zself.phota(parfile,debug=debug)
+        return
 
     def config(self):
         print("="*30)

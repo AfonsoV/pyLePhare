6d6c7671f7eabcba7f9743e087aca31bf835cae9
AfonsoV <bribeiro@dhcp188.strw.leidenuniv.nl>
2019-07-24 12:26:02 +0200

    Added capabilities to lephare, including input table from astropy.Table

--- lephare/lephare.py
+++ lephare/lephare.py
@@ -13,6 +13,10 @@ import sys
 import subprocess as sp
 import os
 import copy
+import glob
+import tempfile
+
+from astropy import units as u
 
 from .default import default_pars, default_config
 
@@ -62,6 +66,26 @@ class LePhare:
         os.environ["LEPHAREWORK"] = dir
         self.workdir = dir
         return
+
+    def get_filters(self,filters,SearchFolder=None):
+        lepharedir = os.environ['LEPHAREDIR']
+        if SearchFolder is None:
+            SearchFolder = "*"
+
+        filter_files = []
+        for fltr in filters:
+            searchPattern = f"{lepharedir}/filt/{SearchFolder}/*{fltr}*"
+            file = glob.glob(searchPattern)
+            if len(file) == 0:
+                raise lephareError(f"Filter {fltr} file not found in {lepharedir}/filt/{SearchFolder}.")
+            filter_files.append( "/".join(file[0].split("/")[-2:]) )
+
+        return filter_files
+
+    def set_filters(self,filters,SearchFolder=None):
+        filter_files = self.get_filters(filters,SearchFolder=SearchFolder)
+        LePhare.CONFIG["FILTER_LIST"] = ",".join(filter_files)
+        return None
 
     def run_cmd(self,cmd,debug=False):
         result = sp.run(cmd,shell=True,stdout=sp.PIPE)
@@ -133,11 +157,46 @@ class LePhare:
         zself.phota(parfile,debug=debug)
         return
 
+    def create_lephare_input(self,table,bands,tmpdir=None,errsuffix="_err"):
+        if tmpdir is None:
+            tmpdir = os.getcwd()
+
+        fname = f"{tmpdir}/lephare{tempfile.gettempprefix()}"
+
+
+        if not "ID" in table.colnames:
+            table["ID"] = np.arange(1,len(table)+1,dtype=np.int)
+        elif table["ID"].dtype == int:
+            pass
+        else:
+            raise lephareError("ID column must be of integer type.")
+
+        cols = ["ID"]
+        for bnd in bands:
+            if not bnd in table.colnames:
+                raise lephareError(f"{bnd} not found in input table.")
+
+            if not f"{bnd}{errsuffix}" in table.colnames:
+                raise lephareError(f"{bnd}{errsuffix} not found in input table. Consider changing errsuffix {errsuffix} to match table names.")
+
+            if table[bnd].unit is None:
+                raise lephareError(f"{bnd} column has no units.")
+            elif table[bnd].unit is u.mag:
+                pass
+            else:
+                table[bnd].convert_unit_to("erg/(s cm2 Hz)")
+                cols.append(bnd)
+                table[f"{bnd}{errsuffix}"].convert_unit_to("erg/(s cm2 Hz)")
+                cols.append(f"{bnd}{errsuffix}")
+
+        table[cols].write(fname,format="ascii.commented_header")
+        return fname
+
     def config(self):
         print("="*30)
         print("Current configuration parameters")
         print("="*30)
-        for key, val in lePhare.CONFIG.items():
+        for key, val in LePhare.CONFIG.items():
             print(f"{key}:\t{val}")
         return
 
@@ -145,7 +204,7 @@ class LePhare:
         print("="*30)
         print("Current output parameters")
         print("="*30)
-        for key, val in lePhare.PARAMETERS.items():
+        for key, val in LePhare.PARAMETERS.items():
             if val == 1 :
                 print(f"{key}")
         return
@@ -153,7 +212,7 @@ class LePhare:
 
 if __name__ == "__main__" :
 
-    run1 = lePhare(config = {"ZFIX":"YES"})
+    run1 = LePhare(config = {"ZFIX":"YES"})
     # run1.config()
     # run1.param()
     run1.set_working_directory("/Users/bribeiro/git-projects/pyLePhare")
